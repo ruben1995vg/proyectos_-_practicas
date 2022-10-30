9c89d1e153f0cee9488da8f493099d163c00bc97
ruben1995vg <ruben1995.vg@gmail.com>
2022-10-30 14:16:54 +0100

    ciclo while añadido al juego

--- juego.py
+++ juego.py
@@ -2,6 +2,7 @@ import getpass
 import random
 
 opciones = {1: 'Piedra', 2: 'Papel', 3: 'Tijera'}
+empate = True
 
 
 def tex():
@@ -9,21 +10,27 @@ def tex():
     print(f'El PC ha elegido: {opciones[pc]}\n')
 
 
-print('\njuego de Piedra, Papel o Tijera \n')
-usuario = int(
-    input('ingrese: \n1 para Piedra \n2 para Papel \n3 para Tijera => '))
-pc = random.randint(1, 3)
-if pc == usuario:
-    tex()
-    print('Empate')
-elif (usuario == 1 and pc == 3) or (usuario == 2 and pc == 1) or (usuario == 3 and pc == 2):
-    tex()
-    print('Has ganado')
-elif usuario not in range(1, 3):
-    tex()
-    print('Has perdido')
-else:
-    tex()
-    print('Has perdido')
+while empate == True:
+
+    print('\njuego de Piedra, Papel o Tijera \n')
+    usuario = int(
+        input('ingrese: \n1 para Piedra \n2 para Papel \n3 para Tijera => '))
+    pc = random.randint(1, 3)
+    if pc == usuario:
+        tex()
+        empate = True
+        print('Empate')
+    elif (usuario == 1 and pc == 3) or (usuario == 2 and pc == 1) or (usuario == 3 and pc == 2):
+        tex()
+        empate = False
+        print('Has ganado')
+    elif usuario not in range(1, 3):
+        tex()
+        empate = False
+        print('Has perdido')
+    else:
+        tex()
+        empate = False
+        print('Has perdido')
 
 getpass.getpass('press enter')
